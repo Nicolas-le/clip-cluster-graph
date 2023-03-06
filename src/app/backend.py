efcea1af8e04e356cf31120b9a29962f01782def
Nicolas-le <nicolas.ruth@web.de>
2023-03-06 17:14:40 +0100

    show communities try

--- src/app/backend.py
+++ src/app/backend.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    graph = prepare_graph_data("./outputs/27_01_2023_07_53_00/graph.json")
+    graph = prepare_graph_data("./outputs/27_01_2023_07_53_00/graph_communities.json")
     return render_template('graph.html', graph = graph)
 
 if __name__ == "__main__":
